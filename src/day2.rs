ff9f4fd78c3bb8d6c6e545f8c5d0d81e026f1012
Tom Watson <tom.watson@kinesis.org>
2021-01-01 19:14:27 +1100

    day2 part 1

---
+++ src/day2.rs
@@ -0,0 +1,79 @@
+use std::{
+    path::PathBuf,
+    fs::File,
+    io::{BufReader, BufRead}
+};
+use regex::Regex;
+
+struct Policy {
+    pub lb: i32,
+    pub ub: i32,
+    pub letter: char
+}
+
+impl Policy {
+    fn from_str(s: &str) -> Result<Policy, &'static str> {
+        let re = Regex::new(r"(\d+)-(\d+) ([a-z])").unwrap();
+        let cap = re.captures_iter(s).next()
+            .ok_or("Regex didn't work")?;
+        let lb: i32 = cap[1].parse::<i32>()
+            .map_err(|_| "Couldn't parse lower bound")?;
+        let ub: i32 = cap[2].parse::<i32>()
+            .map_err(|_| "Couldn't parse upper bound")?;
+        let letter: char = cap[3].chars().nth(0)
+            .ok_or("Couldn't get the letter")?;
+        Ok(Policy { lb: lb, ub: ub, letter: letter })
+    }
+
+    fn is_valid(&self, password: &str) -> bool {
+        let letter_count: i32 = password.chars()
+            .map(|c| if c == self.letter { 1 } else { 0 })
+            .sum();
+        (self.lb..=self.ub).contains(&letter_count)
+    }
+}
+
+fn valid_password(line: String) -> Result<bool, &'static str> {
+    if let Some((policy_str, password)) = line.split_once(':') {
+        let policy = Policy::from_str(&policy_str[..]);
+        let password = password.trim_start();
+        policy.map(|p| p.is_valid(password))
+    } else {
+        Err("Failed to interpret line")
+    }
+}
+
+pub fn run(data_dir: &PathBuf) -> Result<String, &'static str> {
+    let file = File::open(data_dir.join("day2-input.txt"))
+        .map_err(|_| "Couldn't open the file")?;
+    BufReader::new(file)
+        .lines()
+        .map(|x| x.map_err(|_| "Couldn't read line").map(valid_password).flatten())
+        .collect::<Result<Vec<_>, _>>()
+        .map(|v| v.into_iter().filter(|&x| x).count())
+        .map(|x| format!("{}", x))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_policy() {
+        let p = Policy::from_str("3-4 l").unwrap();
+        assert_eq!(p.lb, 3);
+        assert_eq!(p.ub, 4);
+        assert_eq!(p.letter, 'l');
+        
+        let p = Policy::from_str("10-20 x").unwrap();
+        assert_eq!(p.lb, 10);
+        assert_eq!(p.ub, 20);
+        assert_eq!(p.letter, 'x');
+    }
+
+    #[test]
+    fn test_password_validity() {
+        let p = Policy::from_str("3-4 l").unwrap();
+        assert!(p.is_valid("absdfhlasdflasdfl"));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+#![feature(str_split_once, result_flattening)]
+
 pub mod day1;
+pub mod day2;
 
 use std::{
     env,
@@ -7,6 +10,9 @@ use std::{
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Where's the data directory arg?!?!")
+    }
     let data_dir: PathBuf = (&args[1]).into();
 
     println!("Well hello there, let's do the advent of code");
@@ -14,5 +20,11 @@ fn main() {
     println!("\t{}", day1::run(&data_dir));
 
     println!("Day 1, part 2...");
-    print!("\t{}", day1::run_part2(&data_dir));
+    println!("\t{}", day1::run_part2(&data_dir));
+
+    println!("Day 2 ...");
+    match day2::run(&data_dir) {
+        Ok(s) => println!("\t{}", s),
+        Err(e) => println!("\tERROR: {}", e)
+    };
 }
