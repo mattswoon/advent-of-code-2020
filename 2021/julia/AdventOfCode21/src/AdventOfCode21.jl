2b85366a6216e97a65c783d2d1d1612f3aba893e
Tom Watson <tom.watson@kinesis.org>
2021-12-03 10:30:50 +1100

    day1 part 2

--- 2021/julia/AdventOfCode21/src/AdventOfCode21.jl
+++ 2021/julia/AdventOfCode21/src/AdventOfCode21.jl
@@ -3,8 +3,13 @@ module AdventOfCode21
 include("./day1.jl")
 
 function run(data_dir::String)
-    printstyled("Day 1:", bold=true, color=:green)
-    print("\t\t$(Day1.run(data_dir))")
+    run_bit("Day 1 (part 1)", Day1.run_part1, data_dir)
+    run_bit("Day 1 (part 2)", Day1.run_part2, data_dir)
+end
+
+function run_bit(name::String, func::Function, data_dir::String)
+    printstyled("$name:", bold=true, color=:green)
+    println("\t\t$(func(data_dir))")
 end
 
 end # module
--- 2021/julia/AdventOfCode21/src/day1.jl
+++ 2021/julia/AdventOfCode21/src/day1.jl
@@ -1,9 +1,54 @@
 module Day1
 
+using Base.Iterators
+
 mutable struct Scanner
     prev::Int64
     count::Int64
 end
+
+struct WindowIterator{I}
+    iter::I
+    size::Integer
+end
+
+function Base.iterate(w::WindowIterator{I}) where I
+    result = iterate(w.iter)
+    if result == nothing
+        return nothing
+    end
+    (e, state) = result
+    len = min(w.size, length(w.iter))
+    if len == 0
+        return nothing
+    end
+    window = Vector{eltype(w.iter)}(undef, len)
+    window[1] = e
+    for i in 2:length(window)
+        (e, state) = iterate(w.iter, state)
+        window[i] = e
+    end
+    (window, (window, state))
+end
+
+function Base.iterate(w::WindowIterator{I}, state) where I
+    (window, state) = state
+    result = iterate(w.iter, state)
+    if result == nothing
+        return nothing
+    else
+        (next_elem, state) = result
+        window = vcat(window[2:w.size], [next_elem])
+        return (window, (window, state))
+    end
+end
+
+Base.IteratorSize(::WindowIterator{I}) where I = Base.HasLength()
+Base.IteratorEltype(::WindowIterator{I}) where I = Base.HasEltype()
+Base.eltype(w::WindowIterator{I}) where I = Vector{eltype(I)}
+Base.length(w::WindowIterator{I}) where I = length(w.iter) == 0 ? 0 : max(length(w.iter) - w.size + 1, 1)
+
+window(iter::I, size::Integer) where I = WindowIterator(iter, size)
 
 cmp(s::Scanner, value::Int64) = s.prev < value
 function tick!(s::Scanner, value::Int64)
@@ -14,9 +59,10 @@ function tick!(s::Scanner, value::Int64)
     s
 end
 
-function num_increases(data::Array{Int64})::Int64 
-    scan = Scanner(first(data), 0)
-    for x in data[2:end]
+function num_increases(itr)
+    (first, rest) = Iterators.peel(itr)
+    scan = Scanner(first, 0)
+    for x in rest
         tick!(scan, x)
     end
     scan.count
@@ -26,6 +72,11 @@ function read(f::String)::Array{Int64}
     map(x -> parse(Int64, x), readlines(f))
 end
 
-run(data_dir::String) = joinpath(data_dir, "day1.txt") |> read |> num_increases
+run_part1(data_dir::String) = joinpath(data_dir, "day1.txt") |> read |> num_increases
 
+run_part2(data_dir::String) = joinpath(data_dir, "day1.txt") |> 
+    read |> 
+    arr -> window(arr, 3) |>
+    x -> Iterators.map(sum, x) |>
+    num_increases
 end
