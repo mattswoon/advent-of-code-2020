16948152538346055cdc65aac9dab28e370c52b1
Tom Watson <tom.watson@kinesis.org>
2021-12-05 22:45:14 +1100

    day4 part 1

--- 2021/julia/AdventOfCode21/src/AdventOfCode21.jl
+++ 2021/julia/AdventOfCode21/src/AdventOfCode21.jl
@@ -3,6 +3,7 @@ module AdventOfCode21
 include("./day1.jl")
 include("./day2.jl")
 include("./day3.jl")
+include("./day4.jl")
 
 function run(data_dir::String)
     run_bit("Day 1 (part 1)", Day1.run_part1, data_dir)
@@ -11,6 +12,7 @@ function run(data_dir::String)
     run_bit("Day 2 (part 2)", Day2.run_part2, data_dir)
     run_bit("Day 3 (part 1)", Day3.run_part1, data_dir)
     run_bit("Day 3 (part 2)", Day3.run_part2, data_dir)
+    run_bit("Day 4 (part 1)", Day4.run_part1, data_dir)
 end
 
 function run_bit(name::String, func::Function, data_dir::String)
---
+++ 2021/julia/AdventOfCode21/src/day4.jl
@@ -0,0 +1,57 @@
+module Day4
+
+mutable struct BingoBoard
+    x::Array{Int64, 2}
+    checked::Array{Bool, 2}
+end
+
+function Base.parse(::Type{BingoBoard}, s)
+    s = strip(s)
+    b = Array{Int64, 2}(undef, 5, 5)
+    checked = zeros(Bool, 5, 5)
+    for (i, line) in enumerate(split(s, "\n"))
+        row = split(line, " ") |>
+            x -> filter(!isempty, x) |> 
+            x -> parse.(Int64, x)
+        b[i, :] = row
+    end
+    BingoBoard(b, checked)
+end
+
+function read(f::String)
+    s = Base.read(f, String) |> s -> split(s, "\n\n")
+    numbers = popfirst!(s) |> 
+        a -> split(a, ",") |>
+        a -> parse.(Int64, a)
+    boards = parse.(BingoBoard, s)
+    (boards, numbers)
+end
+
+function mark!(board::BingoBoard, value::Int64)
+    board.checked = board.checked .| (board.x .== value)
+    board
+end
+
+check(board::BingoBoard) = (sum(board.checked, dims=1) |> a -> any(x -> x == 5, a)) |
+    (sum(board.checked, dims=2) |> a -> any(x -> x == 5, a))
+
+score(board::BingoBoard, last::Int64) = sum(.!(board.checked) .* board.x) * last
+
+function first_winning_board!(boards::Vector{BingoBoard}, numbers::Vector{Int64})
+    for num in numbers
+        mark!.(boards, num)
+        idx = findfirst(check, boards)
+        if idx != nothing
+            return (boards[idx], num)
+        end
+    end
+    nothing
+end
+
+run_part1(data_dir::String) = joinpath(data_dir, "day4.txt") |>
+    read |>
+    a -> first_winning_board!(a...) |>
+    a -> score(a...)
+
+
+end # module
